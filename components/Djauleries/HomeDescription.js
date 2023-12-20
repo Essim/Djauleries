8d1456b374031455e81dc695de0fed249c47ec61
Essim <essimcaldin@gmail.com>
2023-12-20 09:34:25 +0100

    [FEAT] Home text

--- components/Djauleries/HomeDescription.js
+++ components/Djauleries/HomeDescription.js
@@ -47,6 +47,7 @@ class HomeDescription extends Component {
               background-color: black;
               display: inline-block;
               box-shadow: rgba(0, 0, 0, 0.8) 0px 0px 10px 10px;
+              text-align: center;
             }
             .subTitleLine {
               margin-left: 20%;
@@ -61,25 +62,24 @@ class HomeDescription extends Component {
           `}
         </style>
         <div className="subTitle">Accueil</div>
-        <div className="textBlock">
-          Bienvenue dans cette première
-          <a
-            href="https://forums.jeuxonline.info/sujet/1333946/djauleries-tournoi-mensuel"
-            target="_blank"
-          >
-            {"(?)"}
-          </a>
-          édition des Djauleries !
-        </div>
+        <div className="textBlock">Bienvenue aux Djauleries !</div>
 
         <div className="textBlock">
-          Les Djauleries sont un tournoi mensuel se déroulant sur le tournoi, en
-          association avec le{" "}
+          Les Djauleries sont un tournoi mensuel, se déroulant sur le serveur
+          tournoi de Dofus, en association avec le
           <a href="https://ktarena.com/" target="_blank">
             KTA
           </a>
-          . C'est l'occasion pour tout le monde de s'amuser et s'entraîner, avec
-          des horaires flexibles et des règles originales.
+          .
+        </div>
+
+        <div className="textBlock">
+          C'est l'occasion pour tous les participants de s'amuser et
+          s'entraîner, avec des horaires flexibles et des règles originales.
+        </div>
+        
+        <div className="textBlock">
+          Les Djauleries sont organisées par Shallio et Swipy, n'hésitez pas à nous contacter via Discord en cas de question.
         </div>
       </div>
     );
